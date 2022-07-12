164499b30d6c3d03aa6d7a55e4e8fa189bd3419a
Sergey Kravchenko <s.kravchenko@outlook.com>
2022-07-12 22:25:34 +0300

    feat(task-4): init db config

--- product-service/serverless.ts
+++ product-service/serverless.ts
@@ -7,6 +7,7 @@ import getProductById from "@functions/getProductById";
 const serverlessConfiguration: AWS = {
   service: "product-service",
   frameworkVersion: "3",
+  useDotenv: true,
   plugins: ["serverless-auto-swagger", "serverless-offline", "serverless-esbuild"],
   provider: {
     name: "aws",
@@ -20,6 +21,11 @@ const serverlessConfiguration: AWS = {
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
       NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
+      PG_HOST: "shop-aws-db-instance.cdvypvjonlyd.eu-west-1.rds.amazonaws.com",
+      PG_PORT: "5432",
+      PG_DATABASE: "shop_aws_db",
+      PG_USERNAME: "postgres",
+      PG_PASSWORD: "${env:PG_PASSWORD}",
     },
   },
   // import the function via paths
